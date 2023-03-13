b024229617fab9dccba588eadb12b06438d2b95c
Jason Freeberg <jason.freeberg@snowflake.com>
2023-03-13 13:54:54 -0700

    Format code

--- src/test/scala/org/example/procedure/AppTest.scala
+++ src/test/scala/org/example/procedure/AppTest.scala
@@ -7,17 +7,17 @@ import org.scalatest.funspec.AnyFunSpec
 import org.example.util.LocalSession
 
 class AppTest extends AnyFunSpec {
-  
-    describe("The App procedure") {
-        describe("Given a connection to Snowflake") {
-            val conn = LocalSession.getLocalSession()
 
-            it("Should return 2") {
-                val expected: Long = 2
-                val actual = App.run(conn)
-                assert(expected == actual)
-            }
-        }
+  describe("The App procedure") {
+    describe("Given a connection to Snowflake") {
+      val conn = LocalSession.getLocalSession()
+
+      it("Should return 2") {
+        val expected: Long = 2
+        val actual = App.run(conn)
+        assert(expected == actual)
+      }
     }
+  }
 
 }
