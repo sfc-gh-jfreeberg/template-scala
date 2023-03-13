acefe8e73ad8bfd907bd11845f58c45078115ca0
Jason Freeberg <jason.freeberg@snowflake.com>
2023-03-13 15:58:36 -0700

    Rename artifact, use variables in resources.sql

--- resources.sql
+++ resources.sql
@@ -1,11 +1,13 @@
 
+!variables
+
 CREATE STAGE IF NOT EXISTS artifacts;
 
-PUT file://*.jar @artifacts AUTO_COMPRESS=FALSE OVERWRITE=TRUE;
+PUT file://&artifact_name @artifacts AUTO_COMPRESS=FALSE OVERWRITE=TRUE;
 
-CREATE OR REPLACE PROCEDURE MYPROC(msg STRING)
-  RETURNS STRING
+CREATE OR REPLACE PROCEDURE MYPROC()
+  RETURNS NUMBER
   LANGUAGE SCALA
   RUNTIME_VERSION = '2.12'
-  IMPORTS = ('@artifacts/scala-template-assembly-0.1.0.jar')
+  IMPORTS = ('@artifacts/&artifact_name')
   HANDLER = 'org.example.procedure.App.run';
